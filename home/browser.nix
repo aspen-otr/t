5a26b48336ebb7037bbeabf0b78ce09ff1df3d75
Aspen Price <aspen@aspen-otr.com>
2026-06-12 21:56:51 -0500

    Firefox theme

--- home/browser.nix
+++ home/browser.nix
@@ -8,8 +8,36 @@
 
       configPath = "${config.xdg.configHome}/mozilla/firefox";
 
+
+      policies = {
+        BlockAboutConfig = false;
+        DisableFirefoxAccounts = true;
+        DisableTelemetry = true;
+        ExtensionSettings = {
+          "uBlock0@raymondhill.net" = {
+            default_area = "menupanel";
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+            installation_mode = "force_installed";
+            private_browsing = true;
+          };
+
+          "{3c9f3c5d-ec80-455f-bd7c-4f2b51a1a4d9}" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/{3c9f3c5d-ec80-455f-bd7c-4f2b51a1a4d9}/latest.xpi";
+            installation_mode = "force_installed";
+          };
+        };
+      };
+
       profiles.${username} = {
         name = username;
+
+        extensions.force = true;
+        search.force = true;
+        containersForce = true;
+
+        settings = {
+          "extensions.activeThemeId" = "{3c9f3c5d-ec80-455f-bd7c-4f2b51a1a4d9}";
+        };
 
         search.engines = {
           nix-packages = {
